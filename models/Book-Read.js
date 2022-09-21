33d072806dc82e9cb4e3b86a1804e7065e85de66
alan <cvinny33@gmail.com>
2022-09-20 19:25:14 -0600

    Books-Read table outline

--- models/Book-Read.js
+++ models/Book-Read.js
@@ -1,2 +1,22 @@
 // Will need to reference the User id to keep track of what books that user has read.
 // Will need to be able to tell if the user has already made a review about that book and not allow them to make more than 1 review per book.
+const { UUIDV4, Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Books_Read extends Model { }
+
+Books_Read.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      review_id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+      },
+
+})
+Model.exports = Books_Read
