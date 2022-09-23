2475e18c0f6529fa23dd4c326b1389d298b613d0
alan <cvinny33@gmail.com>
2022-09-22 20:16:09 -0600

    fixed duplicate lines

--- models/index.js
+++ models/index.js
@@ -5,14 +5,10 @@ const User = require('./User');
 
 const Review = require('./Reviews');
 
-module.exports = { User, Read, Books_Read, Review };
-
-const Reviews = require('./Reviews')
-
 Driver.hasOne(License, {
     foreignKey: 'driver_id',
     onDelete: 'CASCADE',
   });
 
-module.exports = { User, Read, Books_Read, Reviews };
+module.exports = { User, Read, Books_Read, Review };
 
