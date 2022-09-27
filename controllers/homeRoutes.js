6125e59621d8d93d03ea08e072c02ace70c9e0bf
Phillip R <philliprosejob@gmail.com>
2022-09-26 20:16:14 -0600

    save before TA help

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Review } = require('../models/index');
 
 router.get('/', async (req, res) => {
   try {
     // Get all users, sorted by name
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
-      order: [['user_name', 'ASC']],
     });
 
     // Serialize user data so templates can read it
@@ -18,5 +17,13 @@ router.get('/', async (req, res) => {
     res.status(500).json(err.message);
   }
 });
+
+router.get('/reviews/:user', async (req, res)=>{
+  try{
+    const userReviews = await Review.findAll({order: [['book_title', 'ASC']]})
+    const reviewList = userReviews.map((reviewContent) => reviewContent.get({ plain: true }));
+    res.render('manageReview', {reviewList})
+  }catch (err){res.status(404).json(err.message)}
+})
 
 module.exports = router;
