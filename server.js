e0a07f9c373530d3a88e325645a404aa2e064a46
Phillip R <philliprosejob@gmail.com>
2022-09-29 18:58:23 -0600

    final build

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')
 (session.Store);
 
+const hbs = exphbs.create({ helpers });
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
 const sess = {
